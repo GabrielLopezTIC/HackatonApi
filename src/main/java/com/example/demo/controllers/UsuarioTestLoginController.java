ff9b23b8d0037e457197dee13e9db3f79ba8c732
GabrielLopezTIC <gabriellopeztic@gmail.com>
2021-10-22 13:46:13 -0500

    se añade cors

--- src/main/java/com/example/demo/controllers/UsuarioTestLoginController.java
+++ src/main/java/com/example/demo/controllers/UsuarioTestLoginController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.example.demo.repository.IUsuarioTestLoginRepo;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 public class UsuarioTestLoginController {
 
 	@Autowired
