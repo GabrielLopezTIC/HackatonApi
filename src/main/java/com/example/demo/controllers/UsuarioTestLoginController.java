7503365810981181f032d3627d48738e840f7ee5
GabrielLopezTIC <gabriellopeztic@gmail.com>
2021-10-22 13:55:39 -0500

    se añade nuevo endpoint get

--- src/main/java/com/example/demo/controllers/UsuarioTestLoginController.java
+++ src/main/java/com/example/demo/controllers/UsuarioTestLoginController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +34,10 @@ public class UsuarioTestLoginController {
 		}else {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario o contraseña incorrectos");
 		}
+	}
+	
+	@GetMapping("/v1/all")
+	public ResponseEntity<List<UsuarioTestLogin>> getAll(){
+		return ResponseEntity.status(HttpStatus.OK).body(usrRepo.findAll());
 	}
 }
